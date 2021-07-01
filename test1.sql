03b3248f5027445798473104fbe9bddd0afd768f
heohitex <73417991+heohitex@users.noreply.github.com>
2021-07-01 20:11:22 +0900

    Update test1.sql

--- test1.sql
+++ test1.sql
@@ -1,3 +1,8 @@
 select distinct city
 from station
 where lower(SUBSTR(city,-1,1)) not in ('a','e','i','o','u');
+
+#
+SQL 구문 연습
+https://www.hackerrank.com/challenges/weather-observation-station-10/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+#
