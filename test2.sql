3fc9c6bab19272874ecb81cddafbcb99c3e5f85e
heohitex <73417991+heohitex@users.noreply.github.com>
2021-07-05 16:37:53 +0900

    Update test2.sql

--- test2.sql
+++ test2.sql
@@ -7,5 +7,7 @@ and months < 10
 order by employee_id asc;
 
 ##
+SQL 구문 연습
+
 https://www.hackerrank.com/challenges/salary-of-employees/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 ##
