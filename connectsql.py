a1dc037a4cae207f9cf271a523927e582650d41d
heohitex <73417991+heohitex@users.noreply.github.com>
2021-07-06 16:02:51 +0900

    Update connectsql.py

--- connectsql.py
+++ connectsql.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import cx_Oracle as oci
-conn = oci.connect('HR/hr@localhost:1521/xepdb1')
+conn = oci.connect('HR/hr@localhost:1521/xepdb1')  # 사용자이름/Password 포트/서비스이름
 print(conn.version)
 df = pd.read_sql("select * from EDA_1",con=conn)
 df
